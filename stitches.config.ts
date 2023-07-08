f60b56b36443fde9ced40dd32a6de03f9ccb4f9d
Vitor Santos <pireshugo737@gmail.com>
2023-07-08 11:57:04 -0300

    :lipstick: Style: stitches
    add initial settings for stitches styles and global styles

---
+++ stitches.config.ts
@@ -0,0 +1,42 @@
+import { createStitches, globalCss } from '@stitches/react';
+
+export const stitches = createStitches({
+  theme: {
+    colors: {
+      brand: '#da2535',
+      primaryDark: '#292929',
+      darkLighter: '#616161',
+      white: '#fbfbfb',
+    },
+
+    fonts: {
+      title: 'Montserrat, sans-serif',
+      text: 'Inter, sans-serif',
+    },
+
+    fontWeights: {
+      bold: 700,
+      semiBold: 600,
+      medium: 500,
+      normal: 400,
+    },
+  },
+});
+
+globalCss({
+  '*': {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box',
+  },
+
+  'a, button': {
+    fontFamily: 'inherit',
+    color: 'inherit',
+  },
+  a: { textDecoration: 'none' },
+
+  ul: { listStyle: 'none' },
+
+  '.flex': { display: 'flex' },
+})();
